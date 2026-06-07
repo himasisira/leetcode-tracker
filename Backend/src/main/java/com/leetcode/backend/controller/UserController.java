dd5d32d89370bb53d162437dcbbb0020ffc0ca7c
himasisira <himasisira30@gmail.com>
2026-06-07 18:36:39 +0530

    Update UserController CORS

--- Backend/src/main/java/com/leetcode/backend/controller/UserController.java
+++ Backend/src/main/java/com/leetcode/backend/controller/UserController.java
@@ -7,7 +7,13 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(
+        origins = {
+                "http://localhost:3000",
+                "http://localhost:3001",
+                "https://leetcode-tracker-brown-one.vercel.app"
+        }
+)
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -15,35 +21,29 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    // REGISTER USER
     @PostMapping("/register")
     public User registerUser(@RequestBody User user) {
         return userService.saveUser(user);
     }
 
-    // LOGIN USER
     @PostMapping("/login")
     public User loginUser(@RequestBody User loginData) {
-
         return userService.loginUser(
                 loginData.getEmail(),
                 loginData.getPassword()
         );
     }
 
-    // GET ALL USERS
     @GetMapping
     public List<User> getAllUsers() {
         return userService.getAllUsers();
     }
 
-    // GET USER BY ID
     @GetMapping("/{id}")
     public User getUserById(@PathVariable Long id) {
         return userService.getUserById(id);
     }
 
-    // UPDATE USER
     @PutMapping("/{id}")
     public User updateUser(
             @PathVariable Long id,
@@ -52,7 +52,6 @@ public class UserController {
         return userService.updateUser(id, updatedUser);
     }
 
-    // DELETE USER
     @DeleteMapping("/{id}")
     public String deleteUser(@PathVariable Long id) {
         return userService.deleteUser(id);
